a707d05fc33f34ecd911960cc9689bf27a4d2cc1
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-10-25 00:12:59 -0600

    INICIO PROYECTO

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'inmobitt-cliente',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
