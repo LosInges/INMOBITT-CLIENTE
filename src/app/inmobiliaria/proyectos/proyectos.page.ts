b68b1ad6f2701661658b5c506a45c7ee7e94fdcb
Ximena <xivillegassa@ittepic.edu.mx>
2022-11-19 02:03:14 -0700

    Inmueble/s

--- src/app/inmobiliaria/proyectos/proyectos.page.ts
+++ src/app/inmobiliaria/proyectos/proyectos.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Proyecto } from 'src/app/interfaces/proyecto';
+import { ProyectosService } from 'src/app/services/proyectos.service';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,13 +10,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./proyectos.page.scss'],
 })
 export class ProyectosPage implements OnInit {
-  
+  proyectos: Proyecto[]=[];
 
-  constructor(private activedRoute:ActivatedRoute) { }
+
+  constructor(
+    private activedRoute:ActivatedRoute,
+    private proyectosService: ProyectosService,
+    private sessionService: SessionService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
-    this.activedRoute.params.subscribe(params => {
-      console.log(params)
+    console.log(this.sessionService);
+    this.activatedRoute.params.subscribe((params) => { 
+      if (params.inmobiliaria) {
+        this.proyectosService
+          .getProyectosInmobiliaria(params.inmobiliaria)
+          .subscribe((proyectos) => {
+            this.proyectos = proyectos; 
+            console.log(proyectos)
+          });
+      }
+    });
+  }
+  navegar(proyecto: Proyecto){
+    this.router.navigate(['./',proyecto.nombre,'inmuebles'],{
+      relativeTo: this.activatedRoute
     })
   }
 
