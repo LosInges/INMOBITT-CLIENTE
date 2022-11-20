1e4d34784debc457aaa4ab3d2e8753ce6905b77c
Ximena <xivillegassa@ittepic.edu.mx>
2022-11-20 11:37:30 -0700

    Validacion correo existente

--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Cliente } from 'src/app/interfaces/cliente'; 
-import { ClienteService } from 'src/app/services/cliente.service'; 
+import { Cliente } from 'src/app/interfaces/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { AlertController } from '@ionic/angular';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-signup',
@@ -19,60 +20,102 @@ export class SignupComponent implements OnInit {
   };
   confirmPassword = '';
 
-  constructor(private modalController: ModalController,
+  constructor(
+    private modalController: ModalController,
     private clienteService: ClienteService,
     private alertCtrl: AlertController,
     private router: Router,
-    ) {}
+    private loginService: LoginService
+  ) {}
 
-    async mostrarAlerta(titulo:string, subtitulo:string, mensaje:string) {  
-      const alert = await this.alertCtrl.create({  
-        header: titulo,  
-        subHeader: subtitulo,  
-        message: mensaje,  
-        buttons: ['OK']  
-      });  
-      await alert.present();  
-      const result = await alert.onDidDismiss();  
-      console.log(result);  
-      this.router.navigate(['/', 'login'])
-    } 
+  async mostrarAlerta(titulo: string, subtitulo: string, mensaje: string) {
+    const alert = await this.alertCtrl.create({
+      header: titulo,
+      subHeader: subtitulo,
+      message: mensaje,
+      buttons: ['OK'],
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
+    this.router.navigate(['/', 'login']);
+  }
 
   ngOnInit() {}
 
   onSubmit() {
     if (
-    this.cliente.apellido.trim().length <= 0 ||
-    this.cliente.nombre.trim().length <= 0 ||
-    this.cliente.correo.trim().length <= 0 ||
-    this.cliente.password.trim().length <= 0
-    ){
+      this.cliente.apellido.trim().length <= 0 ||
+      this.cliente.nombre.trim().length <= 0 ||
+      this.cliente.correo.trim().length <= 0 ||
+      this.cliente.password.trim().length <= 0
+    ) {
       this.clienteService.postCliente(this.cliente)?.subscribe((val) => {
         console.log(val);
-        this.mostrarAlerta("Error", "Campos vacios", "No deje espacios en blanco.")
+        this.mostrarAlerta(
+          'Error',
+          'Campos vacios',
+          'No deje espacios en blanco.'
+        );
       });
-    }else{
+    } else {
       if (this.cliente.password === this.confirmPassword) {
-        if(this.cliente.apellido.trim().length>0 &&
-          (this.cliente.correo.match("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-          + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"))&&
-          this.cliente.nombre.trim().length>0 &&
-          this.cliente.password.trim().length >0 )
-  
-          this.clienteService.postCliente(this.cliente).subscribe((respuesta)=>{
-            console.log(respuesta);
-            this.cerrar()
-          })
-            this.clienteService.postCliente(this.cliente)?.subscribe((val) => {
-            console.log(val);
-            this.mostrarAlerta("Completado", "Creación", "Cliente creado exitosamente.")
+        if (
+          this.cliente.apellido.trim().length > 0 &&
+          this.cliente.correo.match(
+            '^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@' +
+              '[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'
+          ) &&
+          this.cliente.nombre.trim().length > 0 &&
+          this.cliente.password.trim().length > 0
+        ) {
+          //validar
+        if (this.confirmPassword === this.cliente.password) {
+        this.loginService
+          .solicitarRegistro(this.cliente.correo)
+          .subscribe((solicitud) => {
+            if (solicitud.permiso) {
+              this.clienteService
+                .postCliente(this.cliente)
+                .subscribe((res) => {
+                  if (res.results) {
+                    this.modalController.dismiss();
+                  } else {
+                    console.log(res);
+                    this.mostrarAlerta(
+                      'Completado',
+                      'Creación',
+                      'Inmobiliaria creada exitosamente.'
+                    );
+                    this.cerrar();
+                  }
+                });
+            } else {
+              this.mostrarAlerta(
+                'Error:',
+                'Correo ya registrado',
+                'Favor de introducir otro correo.'
+              );
+            }
           });
-      }else{
-        this.mostrarAlerta("Error:", "Confirmación de clave incorrecta", "¿es correcta o esta vacia?")
+      } else {
+        this.mostrarAlerta(
+          'Error:',
+          'Confirmación de clave incorrecta',
+          '¿es correcta o esta vacia?'
+        );
       }
-    }
+        }
+      } else {
+        this.mostrarAlerta(
+          'Error',
+          'Campos vacios',
+          'No deje espacios en blanco.'
+        );
+      }
+    } 
   }
-  
+
   cerrar() {
     this.modalController.dismiss();
   }
