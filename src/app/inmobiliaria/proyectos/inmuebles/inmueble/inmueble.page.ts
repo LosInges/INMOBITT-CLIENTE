bdd04995595c4527d0da4d20d346254d553822d8
Ximena <xivillegassa@ittepic.edu.mx>
2022-11-20 14:08:04 -0700

    alerta

--- src/app/inmobiliaria/proyectos/inmuebles/inmueble/inmueble.page.ts
+++ src/app/inmobiliaria/proyectos/inmuebles/inmueble/inmueble.page.ts
@@ -8,6 +8,8 @@ import { environment } from 'src/environments/environment';
 import { Notario } from 'src/app/interfaces/notario';
 import { AgenteService } from 'src/app/services/agente.service';
 import { NotarioService } from 'src/app/services/notario.service';
+import { AlertController, ModalController } from '@ionic/angular';
+import { MapsComponent } from 'src/app/maps/maps.component';
 
 @Component({
   selector: 'app-inmueble',
@@ -64,7 +66,9 @@ export class InmueblePage implements OnInit {
     private inmuebleService: InmuebleService,
     private router: Router,
     private agenteService: AgenteService,
-    private notarioService: NotarioService
+    private notarioService: NotarioService,
+    private modalController: ModalController,
+    private alertCtrl: AlertController
 
   ) { }
 
@@ -90,12 +94,43 @@ export class InmueblePage implements OnInit {
     
   }
 
+  async mostrarAlerta(titulo: string, subtitulo: string, mensaje: string) {
+    const alert = await this.alertCtrl.create({
+      header: titulo,
+      subHeader: subtitulo,
+      message: mensaje,
+      buttons: ['OK'],
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
+    this.router.navigate(['/', 'login']);
+  }
+
+  async verDireccion() {
+    const modal = await this.modalController.create({
+      component: MapsComponent,
+      componentProps: { position: this.inmueble.direccion },
+      cssClass: 'modalGeneral',
+    });
+
+    modal.present();
+  }
+
   solicitar(){
     this.inmuebleService.postInmuebleCliente(this.inmueble).subscribe(val =>{
       if(val.results){
-        console.log('se registró')
+        this.mostrarAlerta(
+          'Exito',
+          'Se registró correctamente',
+          ''
+        );
       }else{
-        console.log('algo falló')
+        this.mostrarAlerta(
+          'Error',
+          'No se registró',
+          ''
+        );
       }
     })
   }
